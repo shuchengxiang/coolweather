fd24d999c41b6c8603e100174242681f8d574ec8
shuchengxiang <993319134@qq.com>
2018-04-17 15:54:37 +0800

    加入显示天气信息功能。

--- app/src/main/java/com/example/shuchengxiang/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/shuchengxiang/coolweather/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.shuchengxiang.coolweather;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 import com.example.shuchengxiang.coolweather.db.City;
 import com.example.shuchengxiang.coolweather.db.County;
 import com.example.shuchengxiang.coolweather.db.Province;
+import com.example.shuchengxiang.coolweather.gson.Weather;
 import com.example.shuchengxiang.coolweather.util.HttpUtil;
 import com.example.shuchengxiang.coolweather.util.Utility;
 
@@ -89,6 +91,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
---
+++ app/src/main/java/com/example/shuchengxiang/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.shuchengxiang.coolweather.gson;
+
+/**
+ * Created by shuchengxiang on 2018/4/16.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
